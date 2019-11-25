ccbfa33fd374d44e7057122a4a24f970929e3f33
Siyuan Huang <45707116+NathanHuang1999@users.noreply.github.com>
2019-11-26 00:47:19 +0800

    Update header_this.h

--- header_this.h
+++ header_this.h
@@ -10,5 +10,10 @@ using namespace std;
 
 //函数声明
 wstring text_input(char* path);
+
 wstring ANSIToUnicode(const std::string & str);
 string UnicodeToANSI(const std::wstring & wstr);
+
+vector<HuffmanTree<wchar_t>> buildForest(vector<record> freq_table);
+
+void coding(HuffmanNode<wchar_t>* root, string code, vector<record_CodeTable>* codeTable);
